7cc63d4962ba27cff2bbb6529425ab108b693f4b
Kevin Batema <kevinbatema@gmail.com>
2020-07-23 22:56:11 +0200

    Finally got releases working

---
+++ morms/src/index.ts
@@ -0,0 +1,10 @@
+// @create-index
+
+export { Form } from './Form';
+export { FormInput } from './FormInput';
+export { IPInput } from './IPInput';
+export { PasswordInput } from './PasswordInput';
+export { SubmitButton } from './SubmitButton';
+export { TextInput } from './TextInput';
+export *  from './validators';
+
